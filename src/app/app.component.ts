c9c61fa0f6e2357013b2353d280c921cf84b62d5
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-06-10 10:35:45 +0200

    Moved Travel Card from app component to travel-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { travel } from 'src/dto/travel.model';
-import { driver } from 'src/models/driver.model';
-import { passenger } from 'src/models/passenger.model';
-import { vehicle } from 'src/models/vehicle.model';
+
 
 @Component({
   selector: 'app-root',
@@ -10,52 +7,8 @@ import { vehicle } from 'src/models/vehicle.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Project';
-
-  Vehicles: vehicle[] = [
-    {model:'Mercedes',year:2022},
-    {model:'Audi',year:2022},
-  ]
-
-  Drivers: driver[] = [
-    {name:'Driver 1',licenseType:'B',licenseExpDate:'01-01-2024'},
-  ]
-
-    Passengers: passenger[] = [
-      {firstName:'Jurgen',lastName:'Lubonja',age:24},
-      {firstName:'Person',lastName:'Number 2',age:26},
-      {firstName:'Person',lastName:'Number 3',age:23},
-    ]
 
 
-
-
-    Travels : travel[] = [
-
-      {
-        driver:this.Drivers[0],
-        vehicle: this.Vehicles[0],
-        passenger: this.Passengers[0],
-        travelDistance:100
-      }
-        ,
-      {
-        driver: {
-          name:'Driver 1',
-          licenseType:'B',
-          licenseExpDate:'01-01-2024'
-        },
-        vehicle: {
-          model:'Mercedes',
-          year:2022
-        },
-        passenger: {
-          firstName:'Jurgen',
-          lastName:'Lubonja',age:24
-        },
-        travelDistance:100
-      },
-    ]
 
 
 }
--- src/app/travel-card/travel-card.component.ts
+++ src/app/travel-card/travel-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {vehicle} from "../../models/vehicle.model";
+import {driver} from "../../models/driver.model";
+import {passenger} from "../../models/passenger.model";
+import {travel} from "../../dto/travel.model";
 
 @Component({
   selector: 'app-travel-card',
@@ -11,5 +15,51 @@ export class TravelCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  Vehicles: vehicle[] = [
+    {model:'Mercedes',year:2022},
+    {model:'Audi',year:2022},
+  ]
+
+  Drivers: driver[] = [
+    {name:'Hamilton',licenseType:'D',licenseExpDate:'20-05-2030'},
+  ]
+
+  Passengers: passenger[] = [
+    {firstName:'Jonelle',lastName:'Ewart',age:24},
+    {firstName:'Deborah',lastName:'Terry',age:26},
+    {firstName:'Kimberley',lastName:'Hancock',age:23},
+  ]
+
+
+
+
+  Travels : travel[] = [
+
+    {
+      driver:this.Drivers[0],
+      vehicle: this.Vehicles[0],
+      passenger: this.Passengers[0],
+      travelDistance:200
+    }
+    ,
+    {
+      driver: {
+        name:'Brady',
+        licenseType:'c',
+        licenseExpDate:'07-11-2040'
+      },
+      vehicle: {
+        model:'Volkswagen',
+        year:2016
+      },
+      passenger: {
+        firstName:'Jurgen',
+        lastName:'Lubonja',age:24
+      },
+      travelDistance:100
+    },
+  ]
 
 }
